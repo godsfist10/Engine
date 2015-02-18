f476ff7b57ab4a5135b0e55c737e9a9d58d19644
Paul <paul.kubik@mymail.champlain.edu>
2015-02-18 11:37:41 -0500

    fixed diffuse shader

--- Graphics 2/EGP300-proj1/Diffuse.cpp
+++ Graphics 2/EGP300-proj1/Diffuse.cpp
@@ -12,8 +12,8 @@ Diffuse::~Diffuse()
 
 void Diffuse::init()
 {
-	Shader::init("DiffuseVertShader.vp", "DiffuseFragShader.fp", true, true);
-
+	mShaderIndex = gltLoadShaderPairWithAttributes("DiffuseVertShader.vp", "DiffuseFragShader.fp", 2, GLT_ATTRIBUTE_VERTEX, "vVertex", GLT_ATTRIBUTE_TEXTURE0, "texCoord");
+	locMVP = glGetUniformLocation(mShaderIndex, "mvpMatrix");
 	baseImageLoc = glGetUniformLocation(mShaderIndex, "baseImage");
 }
 
